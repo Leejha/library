afb4b8713a7d4d9ac2df1086cfc65a87b90d8a89
이재하 <jhl2619@naver.com>
2023-12-25 23:22:59 +0900

    feat: 모듈 export 추가

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -2,7 +2,7 @@ import { useCallback, useState } from "react";
 
 type ReturnTypes = [boolean, () => void];
 
-export default function useToggle(initialValue = false): ReturnTypes {
+export function useToggle(initialValue = false): ReturnTypes {
   const [value, setValue] = useState(initialValue);
 
   const onToggle = useCallback(() => {
