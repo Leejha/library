371a61a0e950ef8e064221fd9d852a7b84ee6096
이재하 <jhl2619@naver.com>
2024-01-28 20:15:22 +0900

    chore: Support cjs and esm

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,13 @@ import teser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 
 export default [
-  // js 번들링
   {
-    // 번들링 기준 파일
     input: 'src/index.ts',
-    // 번들링 결과 파일과 형식(esm -> es modules, cjs -> commonjs)
-    output: [{ file: 'dist/index.js', format: 'cjs' }],
+    output: [
+      { file: 'dist/index.cjs', format: 'cjs' },
+      { file: 'dist/index.mjs', format: 'esm' },
+    ],
+    // ModuleResolution: 'node',
 
     plugins: [typescript(), teser()],
   },
