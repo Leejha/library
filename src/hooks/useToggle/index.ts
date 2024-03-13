4dd5b5413ffe4fe831a3e3b0fe37f86481f44d5d
이재하 <jhl2619@naver.com>
2024-03-13 23:01:25 +0900

    chore: Add comments describing the code

--- src/hooks/useToggle/index.ts
+++ src/hooks/useToggle/index.ts
@@ -2,6 +2,12 @@ import { useCallback, useState } from 'react';
 
 type ReturnTypes = [boolean, () => void];
 
+/**
+ * Return state and function that toggle it.
+ * @param initialValue Value that state should have initially. Default is `false`.
+ * @returns Array with state and function that toggles it.
+ */
+
 export function useToggle(initialValue = false): ReturnTypes {
   const [value, setValue] = useState(initialValue);
 
